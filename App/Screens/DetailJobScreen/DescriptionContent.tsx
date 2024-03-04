c75d652bd468771154de34090d457851bf330ee8
LiamGrosjean <grosjeanliam@gmail.com>
2024-03-04 15:04:42 +0100

    update

--- App/Screens/DetailJobScreen/DescriptionContent.tsx
+++ App/Screens/DetailJobScreen/DescriptionContent.tsx
@@ -4,12 +4,12 @@ import { StyleSheet } from 'react-native'
 import Colors from '../../Utils/Colors';
 
 
-export default function  DescriptionContent(description: string, title: string){
+export default function  DescriptionContent(){
         return (
             <View style={styles.container}>
                 <ScrollView contentContainerStyle={styles.scrollContainer}>
                     <Text style={{ fontSize: 18, fontWeight: '600', color: '#242C5D', marginBottom: 5 }}> Lorem Ipsum </Text>
-                    <Text style={styles.text}>{description}</Text>
+                    <Text style={styles.text}>texte description</Text>
                 </ScrollView>
             </View>
         );
